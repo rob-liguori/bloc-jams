624dcf4480afa7c0591d57c730217b0a235c33fc
Rob Liguori <rob.liguori@aol.com>
2017-06-15 16:04:16 -0400

    Working PlayPause

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -81,8 +81,28 @@
      }
  };
  
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumElvis);
+   
+        songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+   
+        for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
 
 var albums = [albumPicasso, albumMarconi, albumElvis];
 var index = 2;
